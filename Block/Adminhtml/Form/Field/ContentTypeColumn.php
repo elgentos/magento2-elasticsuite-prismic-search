38555605744dbe252e06d1b4ae5fba902747d9b5
Peter Jaap Blaakmeer <peterjaap@elgentos.nl>
2022-07-06 11:56:08 +0200

    Only show content types that have a route defined in configuration

--- Block/Adminhtml/Form/Field/ContentTypeColumn.php
+++ Block/Adminhtml/Form/Field/ContentTypeColumn.php
@@ -4,18 +4,22 @@ declare(strict_types=1);
 
 namespace Elgentos\ElasticsuitePrismicSearch\Block\Adminhtml\Form\Field;
 
-use Elgentos\PrismicIO\Model\Source\ContentTypes;
+use Elgentos\PrismicIO\Model\ResourceModel\Route\Collection;
+use Elgentos\PrismicIO\Model\ResourceModel\Route\CollectionFactory;
 use Magento\Framework\View\Element\Html\Select;
 use Magento\Framework\View\Element\Context;
 
 class ContentTypeColumn extends Select
 {
-    private ContentTypes $contentTypes;
+    private Collection $routeCollection;
 
-    public function __construct(Context $context, ContentTypes $contentTypes, array $data = [])
-    {
+    public function __construct(
+        Context $context,
+        CollectionFactory $routeCollectionFactory,
+        array $data = []
+    ) {
         parent::__construct($context, $data);
-        $this->contentTypes = $contentTypes;
+        $this->routeCollection = $routeCollectionFactory->create();
     }
 
     public function setInputName(string $value): ContentTypeColumn
@@ -39,11 +43,33 @@ class ContentTypeColumn extends Select
         if (!$this->getOptions()) {
             $this->setOptions($this->getSourceOptions());
         }
+
         return parent::_toHtml();
+    }
+
+    protected function _toOptionArray(
+        Collection $collection,
+        string $valueField = 'id',
+        string $labelField = 'name',
+        array $additional = []
+    ): array {
+        $res                 = [];
+        $additional['value'] = $valueField;
+        $additional['label'] = $labelField;
+
+        foreach ($collection as $item) {
+            foreach ($additional as $code => $field) {
+                $data[$code] = $item->getData($field);
+            }
+            $res[] = $data;
+        }
+
+        return $res;
     }
 
     private function getSourceOptions(): array
     {
-        return $this->contentTypes->toOptionArray();
+        return $this->_toOptionArray($this->routeCollection, 'content_type',
+            'title');
     }
 }
